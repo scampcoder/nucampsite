7b5a4ff6efc261f5303dcaa7d14b86d7f69bd20f
Victoria Kee <victoriakee95@gmail.com>
2021-10-11 13:14:09 -0500

    Mv JS to JS folder/file

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//carousel pause/play
+$(function() {
+    $('.carousel').carousel( { interval: 2000 } );
+    $('#carouselButton').click(function() {
+        if ($('#carouselButton').children('i').hasClass("fa-pause")) {
+            $('.carousel').carousel('pause');
+            $('#carouselButton').children('i').removeClass('fa-pause');
+            $('#carouselButton').children('i').addClass('fa-play');
+        } else {
+            $('.carousel').carousel('cycle');
+            $('#carouselButton').children('i').removeClass('fa-play');
+            $('#carouselButton').children('i').addClass('fa-pause');
+        }
+    });
+});
+//end of carousel pause/play
